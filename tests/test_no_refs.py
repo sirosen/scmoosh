6448b289c9969728da713da934e1a372f9b815d2
Stephen Rosen <sirosen0@gmail.com>
2026-09-23 21:39:10 -0500

    Update basic empty doc test

--- tests/test_no_refs.py
+++ tests/test_no_refs.py
@@ -13,7 +13,15 @@ def test_empty_schema() -> None:
     )
 
     s = Scmoosher(SCHEMA_URI)
-
+    s.walk()
     result = s.render()
 
-    assert result == {"$schema": "https://json-schema.org/draft/2020-12/schema"}
+    assert "$ref" in result
+    def_key = result["$ref"].split("/")[-1]
+    assert result == {
+        "$schema": "https://json-schema.org/draft/2020-12/schema",
+        "$ref": f"#/definitions/{def_key}",
+        "definitions": {
+            def_key: {"$schema": "https://json-schema.org/draft/2020-12/schema"},
+        },
+    }
